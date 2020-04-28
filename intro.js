e46bd8fd580cfb63ed3bcdee38aeddcaa74dcf10
BUGTI <khudabakhsh.bugti@powercoders.org>
2020-04-28 09:13:43 +0200

    First commit

---
+++ intro.js
@@ -0,0 +1,17 @@
+ // How to make arrays
+ let vagitable = ["potato", "brokkoli", "tomatos"];
+ console.log(vagitable);
+ let first = vagitable[0];
+ let last = first[vagitable - 1];
+
+
+ // Function
+ function vagitables(item, index, array) {
+     console.log(item, index);
+
+ }
+ // this is how to add an items
+ vagitable.push("Mango", "Banana", "Apple");
+
+ // this is how to remove the items
+ vagitable.splice(0, 2);
