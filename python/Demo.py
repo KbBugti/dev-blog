e48476a0c5bdcfe219645ea7aee40dacdb74b725
BUGTI <khudabakhsh.bugti@powercoders.org>
2020-04-28 10:24:41 +0200

    First commit

---
+++ python/Demo.py
@@ -0,0 +1,40 @@
+Character_name = "KB"
+Character_age = "25"
+Is_Male = False
+print("My friend name: " + Character_name +  ", ")
+print("And his age: " + Character_age + ", ")
+
+
+Character_name = "Valerie"
+Character_age = "age"
+Character_birth = "01.05.1990"
+Character_id = "28893"
+Character_city = "Neuchatel"
+Character_country = "Suisse"
+Is_Female = True
+print("She is very sensitive and her name: " + Character_name + ", ")
+print("her age: " + Character_age + ", ")
+print("her date of birth: " + Character_birth + ", ")
+print("Id number: " + Character_id + ", ")
+print("Address: " + Character_city + ", ")
+print("Nationality: " + Character_country + ", ")
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+
+Num1 = input("Enter a number: ")
+Num2 = input("Enter another number: ")
+result = float(Num1) + float(Num2)
+print(result)
+
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print("Plural noun " + plural_noun)
+print("I love " + celebrity)
+
